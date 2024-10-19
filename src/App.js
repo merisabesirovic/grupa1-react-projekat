ba66734750e5550cfda46212aa580e0df5a949ad
Merisa <besiroviccm@gmail.com>
2024-10-19 22:19:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import Header from "./Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 function App() {
-  const objekat = new Object();
-  objekat.ime = "Kerim";
-
   return (
     <div className="App">
       <Header></Header>
