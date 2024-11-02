5c2db5fde674facb37c2e0d4bdede36aef444ca7
Merisa <besiroviccm@gmail.com>
2024-11-02 09:30:56 +0100

    update

---
+++ src/pages/AboutFLL/AboutFLL.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function AboutFLL() {
+  return (
+    <div>
+      <p className="aboutFLL">
+        First LEGO League takmičenje nije samo događaj, već prava avantura koja
+        nas vodi kroz svet nauke, tehnologije i timskog rada. Kroz svaki izazov,
+        ne samo što učimo o tehnologiji, već i o važnosti saradnje, komunikacije
+        i poverenja. Deljenje radosti uspeha, prevazilaženje izazova zajedno i
+        podrška u teškim trenucima čine nas ne samo boljim timom, već i boljim
+        ljudima. Dok se suočavamo sa problemima i trudimo se da pronađemo
+        kreativna rešenja, učimo ne samo o naučno-tehničkim konceptima, već i o
+        važnosti upornosti, prijateljstva i timskog duha. Svaki korak na ovom
+        putovanju obogaćuje nas novim iskustvima, učenjima i prijateljstvima
+        koja će trajati celog života. Zato hvala našim mentorima na neumornoj
+        podršci, našim prijateljima na inspiraciji i podršci i svim učesnicima
+        koji zajedno sa nama čine FLL takmičenje nezaboravnom avanturom učenja,
+        rasta i prijateljstva!
+      </p>
+    </div>
+  );
+}
--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -5,15 +5,18 @@ import PersonsCard from "../../components/PersonsCard/PersonsCard";
 
 export default function AboutUs() {
   return (
-    <div className="aboutUs">
-      {persons.map((person) => (
-        <PersonsCard
-          imageURL={person.imageURL}
-          fullName={person.fullName}
-          description={person.description}
-          location={person.location}
-        />
-      ))}
+    <div className="wrapper">
+      <h1>Ovo su članovi naše grupe</h1>
+      <div className="aboutUs">
+        {persons.map((person) => (
+          <PersonsCard
+            imageURL={person.imageURL}
+            fullName={person.fullName}
+            description={person.description}
+            location={person.location}
+          />
+        ))}
+      </div>
     </div>
   );
 }
